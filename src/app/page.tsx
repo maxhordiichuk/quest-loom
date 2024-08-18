982bed538efbe2e06d6e666a148bf9a24437bfd4
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2024-08-18 19:57:07 +0200

    Configure lint

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,16 @@
-import {Button} from "@/components/ui/button";
+import { Button } from '@/components/ui/button'
+
+const text = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\n'
+  + 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\n'
+  + 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\n'
+  + 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
 export default function Home() {
   return (
     <main>
       <h1 className="text-3xl">Home Page</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-      </p>
+      <p>{text}</p>
       <Button>Click me</Button>
     </main>
-  );
+  )
 }
