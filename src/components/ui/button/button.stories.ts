8f06d544a004ec7b1922189bbafcd86d136b7631
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2024-08-25 17:34:19 +0200

    Configure Storybook

---
+++ src/components/ui/button/button.stories.ts
@@ -0,0 +1,87 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button, ButtonProps } from './button'
+
+const mockData: ButtonProps = {
+  variant: 'default',
+  size: 'default',
+  children: 'Click me',
+}
+
+const meta = {
+  title: 'Components/UI/Button',
+  component: Button,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'centered',
+  },
+  args: mockData,
+  argTypes: {
+    variant: {
+      options: ['default', 'destructive', 'outline', 'secondary', 'ghost', 'link'],
+      control: { type: 'radio' },
+    },
+    size: {
+      options: ['default', 'sm', 'lg', 'icon'],
+      control: { type: 'radio' },
+    },
+  },
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    variant: 'default',
+  },
+}
+
+export const Destructive: Story = {
+  args: {
+    variant: 'destructive',
+  },
+}
+
+export const Outline: Story = {
+  args: {
+    variant: 'outline',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+  },
+}
+
+export const Ghost: Story = {
+  args: {
+    variant: 'ghost',
+  },
+}
+
+export const Link: Story = {
+  args: {
+    variant: 'link',
+  },
+}
+
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
+}
+
+export const Large: Story = {
+  args: {
+    size: 'lg',
+  },
+}
+
+export const Icon: Story = {
+  args: {
+    children: '🚀',
+    size: 'icon',
+  },
+}
