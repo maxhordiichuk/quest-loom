86dd445cc5dcd2cca28986545268741583ebcd4e
Max Hordiichuk <hordijchuk.m.i@gmail.com>
2024-09-13 20:38:16 +0200

    Fix QuestNew component

--- src/components/client/quest-new/quest-new.tsx
+++ src/components/client/quest-new/quest-new.tsx
@@ -5,7 +5,7 @@ import { QuestForm } from '@/components/client/quest-form'
 export function QuestNew() {
   return (
     <div className="w-full max-w-2xl mx-auto p-6 md:p-8">
-      <QuestForm createQuest={createQuest} />
+      <QuestForm onSubmit={createQuest} />
     </div>
   )
 }
